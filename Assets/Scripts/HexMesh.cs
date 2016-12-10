12f1a70b6634e9d73ee9c03983f0982f0e6c5f94
CamDHall <camerondylan.h@gmail.com>
2016-12-10 00:39:44 -0500

    Git ignore

---
+++ Assets/Scripts/HexMesh.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+[RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
+public class HexMesh : MonoBehaviour {
+    Mesh hexMesh;
+
+    List<Vector3> vertices;
+    List<int> triangles;
+
+    void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+    }
+}
