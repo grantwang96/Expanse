97aafbf9cfe49ca8217ed976ca3d525f9d9a618f
CamDHall <camerondylan.h@gmail.com>
2016-12-09 22:04:53 -0500

    Init

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    private Vector3 Move;
+    private Transform cameraRotate;
+    public float cameraSpeed = 50;
+
+    void Start()
+    {
+
+    }
+	
+	void Update () {
+        Move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Mouse ScrollWheel")*-1, Input.GetAxis("Vertical"));
+        transform.eulerAngles = new Vector3(20 + Input.GetAxis("Mouse ScrollWheel"), 0, 0);
+
+        transform.position += Move * cameraSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+    bool over = false, cityClicked = false;
+
+    public GameObject regionDropDown, cityMenu;
+    private Vector3 hoverScale;
+    float hoverTime, tranistionTime;
+    public float hoverSpeed = 7f, clickTransitionSpeed = 10f;
+
+	void Start () {
+        hoverScale = Vector3.Lerp(new Vector3(0, 0, 1), new Vector3(1,1,1), 0.5f);
+    }
+	
+	void Update () {
+
+        // Debug.Log(cityClicked);
+
+        if (over)
+        {
+            regionDropDown.GetComponent<RectTransform>().localScale = Vector3.Lerp(new Vector3(0, 0, 1), new Vector3(1, 1, 1), hoverSpeed * (Time.time - hoverTime));
+        } else
+        {
+            regionDropDown.GetComponent<RectTransform>().localScale = new Vector3(0, 0, 1);
+        }
+
+          CityMenu();
+    }
+
+    public void OnMouseOver()
+    {
+        over = true;
+        hoverTime = Time.time;
+    }
+
+    public void OnMouseExit()
+    {
+        over = false;
+    }
+
+    void OnMouseDown()
+    {
+        tranistionTime = Time.time;
+        if (!cityClicked)
+        {
+            cityClicked = true;
+        } else
+        {
+            cityClicked = false;
+        }
+    }
+
+    void CityMenu()
+    {
+        var cityPos = cityMenu.GetComponent<RectTransform>().anchoredPosition3D;
+        if (cityClicked)
+        {
+            cityMenu.GetComponent<RectTransform>().anchoredPosition3D = Vector3.Lerp(cityPos, new Vector3(740, 0, 1), hoverSpeed * (Time.time - tranistionTime));
+        }
+
+        if (!cityClicked)
+        {
+            cityMenu.GetComponent<RectTransform>().anchoredPosition3D = Vector3.Lerp(cityPos, new Vector3(1184, 0, 1), 2 * (Time.time - (tranistionTime+5)));
+            Debug.Log(cityClicked);
+        }
+    }
+}
