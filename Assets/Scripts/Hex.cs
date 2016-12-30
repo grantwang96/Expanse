1b066143ea4aaa08647afc0f2735575bc987bcb5
CamDHall <camerondylan.h@gmail.com>
2016-12-30 14:27:07 -0500

    Hex init

--- Assets/Scripts/Hex.cs
+++ Assets/Scripts/Hex.cs
@@ -2,5 +2,36 @@
 using System.Collections;
 
 public class Hex {
+    public GameObject instance;
 
+    // Coords
+    public Vector3 location = new Vector3(0, 0, 0);
+    public Vector3 coordinates;
+
+    // Not sure what this is--- private HexCoordinate hexCoord;
+
+    public static float sideLength = 1;
+
+    // Mesh
+    public Mesh landMesh = new Mesh();
+    public Mesh lakeMesh = new Mesh();
+
+    // Misc
+    public string type;
+
+    //
+    // CONSTRUCTOR
+    //
+    public Hex(Vector3 loc, Vector3 coords, string hexType, HexCoordinate hexCoordinate)
+    {
+        coordinates = coords;
+
+        hexCoord = hexCoordinate;
+
+        type = hexType;
+
+        location = loc;
+
+        CreateLandMesh();
+    }
 }
