c56ce55c438f9c4513655e41895a77087a9aff0c
Grant Wang <yidegrant@gmail.com>
2017-01-08 21:37:22 -0500

    Starting Hexes From Scratch

---
+++ Assets/Scripts/HexTemp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HexTemp : MonoBehaviour {
+
+    public Vector3 location;
+    private static float sideLength = 1;
+    public float unit = (float)Math.Pow((double)1.0, (double)1.0/3.0);
+    public String region;
+    public String resource;
+    public int value;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    
+}
